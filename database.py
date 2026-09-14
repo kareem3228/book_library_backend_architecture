dfd6fc9dec3cb801c3b36c71795e223e67453924
kareem3228 <kareemmagdy9791@gmail.com>
2026-09-14 16:53:43 +0300

    add database url in env and add env.exampl

--- database.py
+++ database.py
@@ -1,8 +1,11 @@
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.ext.asyncio import async_sessionmaker,create_async_engine
+from dotenv import load_dotenv
+import os
+load_dotenv()
 class Base(DeclarativeBase):
     pass
-database_url="postgresql+asyncpg://postgres:postgres123@localhost:5432/library_architecture"
+database_url=os.getenv('DATABASE_URL')
 engine=create_async_engine(database_url)
 asyncsessionlocal=async_sessionmaker(bind=engine,expire_on_commit=False)
 async def get_db():
